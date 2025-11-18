9d045d0f4e2261827ee8d24219e081b267139489
Mohammed Darkaoui <darkaouimeed12@gmail.com>
2025-11-18 17:10:59 +0100

    Enhancing the Ui design &adding the adding workers form

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
- const addworkerBtn = document.getElementById('btn-add');
+// const modal = document.getElementById('employee-modal');
+// const openBtn = document.getElementById('btn-add'); // bouton "Ajouter un employé"
+
+// openBtn.addEventListener('click', () => {
+//   modal.classList.add('is-open');
+// });
+const modal = document.getElementById('employee-modal');
+const openBtn = document.getElementById('btn-add'); 
+
+openBtn.addEventListener('click' , () => {
+    modal.classList.add('is-open');
+});
